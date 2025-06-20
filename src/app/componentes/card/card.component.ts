e7caa57fd7971679dcc8c1dfc93b415ca80a8a85
DeveloperTDF <appdeveloper230379@gmail.com>
2025-06-20 09:00:21 -0300

    trabajando sobre la card

--- src/app/componentes/card/card.component.ts
+++ src/app/componentes/card/card.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
+  imports:[IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle],
   standalone: true,
 })
 export class CardComponent  implements OnInit {
